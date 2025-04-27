18ab2c3195ffc27672be186b9f3592913e02224d
Markku <markku.seppala@gmail.com>
2025-04-27 17:58:09 +0300

    remove start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Aktivoidaan virtuaaliympäristö
-source .venv/bin/activate
-# Käynnistetään gunicorn virtuaaliympäristön sisällä useammalla työprosessilla
-gunicorn wsgi:app --workers=4 --threads=2 --timeout=120
